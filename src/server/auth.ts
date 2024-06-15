c4bc330a402e27f97542eb54866e1486a5ac4a6c
roshanbvadassery <roshanbvadassry@gmail.com>
2024-06-15 12:51:09 +0530

    added logo to auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,6 +48,9 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
+  theme: {
+    logo: '/logo.svg'
+  },
   adapter: DrizzleAdapter(db, createTable) as Adapter,
   providers: [
     // DiscordProvider({
